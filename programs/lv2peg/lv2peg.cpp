78aa6bceb417ffecd46b7a4cfde00884d15cc21a
Lars Luthman <lars.luthman@gmail.com>
2009-10-11 20:25:00 +0200

    Changed error handling in lv2peg to use exceptions, made things more readable

--- programs/lv2peg/lv2peg.cpp
+++ programs/lv2peg/lv2peg.cpp
@@ -26,6 +26,7 @@
 
 #include <cfloat>
 #include <cstdlib>
+#include <stdexcept>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -86,7 +87,7 @@ private:
 /** A wrapper class for librdf_query_results. */
 
 class RDFResults : public RDFPtr<librdf_query_results, 
-				 &librdf_free_query_results> {
+                                 &librdf_free_query_results> {
 public:
   typedef RDFPtr<librdf_query_results, &librdf_free_query_results> Parent;
   RDFResults(librdf_query_results* r) : Parent(r) { }
@@ -128,30 +129,27 @@ typedef RDFPtr<librdf_storage, &librdf_free_storage> RDFStorage;
 librdf_model* parse_file(RDFWorld& world, string const& filename) {
   
   RDFParser parser(librdf_new_parser(world.get(), "turtle", 0, 0));
-  if (!parser) {
-    cerr<<"Failed to initialise Turtle parser."<<endl;
-    return 0;
-  }
+  if (!parser)
+    throw runtime_error("Failed to initialise Turtle parser.");
   
   RDFStorage storage(librdf_new_storage(world.get(), "memory", "storage", 0));
-  if (!storage)  {
-    cerr<<"Failed to initialise RDF storage."<<endl;
-    return 0;
-  }
+  if (!storage)
+    throw runtime_error("Failed to initialise RDF storage.");
   
   librdf_model* model = librdf_new_model(world.get(), storage.get(), 0);
-  if (!model) {
-    cerr<<"Failed to initialise RDF data model."<<endl;
-    return 0;
-  }
+  if (!model)
+    throw runtime_error("Failed to initialise RDF data model.");
   
   RDFUri file_uri(librdf_new_uri_from_filename(world.get(), filename.c_str()));
   if (!file_uri) {
-    cerr<<"Failed to create URI from filename."<<endl;
-    return 0;
+    librdf_free_model(model);
+    throw runtime_error("Failed to create URI from filename.");
   }
-  if (librdf_parser_parse_into_model(parser.get(), file_uri.get(), 0, model))
-    cerr<<"Failed to parse the input file."<<endl;
+
+  if (librdf_parser_parse_into_model(parser.get(), file_uri.get(), 0, model)) {
+    librdf_free_model(model);
+    throw runtime_error("Failed to parse the input file.");
+  }
   
   return model;
 }
@@ -160,25 +158,23 @@ librdf_model* parse_file(RDFWorld& world, string const& filename) {
 /** Convenience function for running a SPARQL query on a model without all the
     librdf nastyness. */
 librdf_query_results* run_query(RDFModel& model, RDFWorld& world,
-				string const& query, string const& base = "") {
+                                string const& query, string const& base = "") {
   
   librdf_uri* base_uri_p = 0;
   if (base != "")
     base_uri_p = 
       librdf_new_uri(world.get(), 
-		     reinterpret_cast<unsigned char const*>(base.c_str()));
+                     reinterpret_cast<unsigned char const*>(base.c_str()));
   RDFUri base_uri(base_uri_p);
   RDFQuery rdf_query(librdf_new_query(world.get(), "sparql", 0,
-				      reinterpret_cast<unsigned char const*>(query.c_str()),
-				      base_uri.get()));
-  if (!rdf_query) {
-    cerr<<"Failed to initialise SPARQL query."<<endl;
-    return 0;
-  }
+                                      reinterpret_cast<unsigned char const*>(query.c_str()),
+                                      base_uri.get()));
+  if (!rdf_query)
+    throw runtime_error("Failed to initialise SPARQL query.");
   librdf_query_results* rdf_results =
     librdf_query_execute(rdf_query.get(), model.get());
   if (!rdf_results)
-    cerr<<"Failed to execute plugin query."<<endl;
+    throw runtime_error("Failed to execute plugin query.");
   
   return rdf_results;
 }
@@ -195,224 +191,215 @@ int main(int argc, char** argv) {
     }
   }
   
-  // print usage info if the parameters are invalid
-  if (argc < 3) {
-    cerr<<"Use like this: lv2peg <input file> <output file> "<<endl;
-    return -1;
-  }
-  
-  // initialise librdf
-  RDFWorld world (librdf_new_world());
-  if (!world) {
-    cerr<<"Failed to initialise librdf."<<endl;
-    return -1;
-  }
-  
-  // parse turtle file
-  RDFModel model(parse_file(world, argv[1]));
-  if (!model)
-    return -1;
-
-  // find all plugins in the file
-  RDFResults 
-    results(run_query(model, world,
-		      "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-		      "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-		      "SELECT DISTINCT ?plugin, ?pegname WHERE { \n"
-		      "?plugin a :Plugin. \n"
-		      "?plugin ll:pegName ?pegname. }"));
-  if (!results)
-    return -1;
-  map<string, string> plugins;
-  while (!results.finished()) {
-    plugins[results.get_uri(0)] = results.get_literal(1);
-    results.next();
-  }
-  
-  // iterate over all plugins
-  map<string, map<int, PortInfo> > info;
-  map<string, string>::const_iterator plug_iter;
-  
-  for (plug_iter = plugins.begin(); plug_iter != plugins.end(); ++plug_iter) {
+  try {
     
-    // query the plugin ports
-    map<int, PortInfo> ports;
-    {
-      RDFResults results(run_query(model, world,
-				   "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-				   "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-				   "SELECT ?index, ?symbol WHERE { \n"
-				   "<>        :port       ?port. \n"
-				   "?port     :index      ?index; \n"
-				   "          :symbol     ?symbol. }",
-				   plug_iter->first));
-      if (!results)
-	return -1;
-      while (!results.finished()) {
-	int port_index = atoi(results.get_literal(0));
-	if (ports.find(port_index) != ports.end()) {
-	  cerr<<"Index "<<port_index<<" is used for more than one port"<<endl;
-	  return -1;
-	}
-	ports[port_index].name = results.get_literal(1);
-	results.next();
+    // print usage info if the parameters are invalid
+    if (argc < 3)
+      throw runtime_error("Use like this: lv2peg <input file> <output file>");
+    
+    // initialise librdf
+    RDFWorld world (librdf_new_world());
+    if (!world)
+      throw runtime_error("Failed to initialise librdf.");
+    
+    // parse turtle file
+    RDFModel model(parse_file(world, argv[1]));
+    
+    // find all plugins in the file
+    RDFResults 
+      results(run_query(model, world,
+                        "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                        "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                        "SELECT DISTINCT ?plugin, ?pegname WHERE { \n"
+                        "?plugin a :Plugin. \n"
+                        "?plugin ll:pegName ?pegname. }"));
+    map<string, string> plugins;
+    while (!results.finished()) {
+      plugins[results.get_uri(0)] = results.get_literal(1);
+      results.next();
+    }
+    
+    // iterate over all plugins
+    map<string, map<int, PortInfo> > info;
+    map<string, string>::const_iterator plug_iter;
+    
+    for (plug_iter = plugins.begin(); plug_iter != plugins.end(); ++plug_iter) {
+      
+      // query the plugin ports
+      map<int, PortInfo> ports;
+      {
+        RDFResults results(run_query(model, world,
+                                     "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                                     "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                                     "SELECT ?index, ?symbol WHERE { \n"
+                                     "<>        :port       ?port. \n"
+                                     "?port     :index      ?index; \n"
+                                     "          :symbol     ?symbol. }",
+                                     plug_iter->first));
+        while (!results.finished()) {
+          int port_index = atoi(results.get_literal(0));
+          if (ports.find(port_index) != ports.end()) {
+            cerr<<"Index "<<port_index<<" is used for more than one port"<<endl;
+            return -1;
+          }
+          ports[port_index].name = results.get_literal(1);
+          results.next();
+        }
+      
+        // check that the port indices are OK
+        map<int, PortInfo>::const_iterator iter;
+        int next = 0;
+        for (iter = ports.begin(); iter != ports.end(); ++iter) {
+          if (iter->first != next) {
+            cerr<<"There was no description of port "<<next
+                <<" in plugin "<<plug_iter->first<<endl;
+            return -1;
+          }
+          ++next;
+        }
       }
       
-      // check that the port indices are OK
+      // get min values
+      {
+        RDFResults results(run_query(model, world,
+                                     "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                                     "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                                     "SELECT ?index, ?min WHERE { \n"
+                                     "<>        :port       ?port. \n"
+                                     "?port     :index      ?index; \n"
+                                     "          :minimum    ?min. }",
+                                     plug_iter->first));
+        while (!results.finished()) {
+          int port_index = atoi(results.get_literal(0));
+          ports[port_index].min = atof(results.get_literal(1));
+          results.next();
+        }
+      }
+      
+      // get max values
+      {
+        RDFResults results(run_query(model, world,
+                                     "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                                     "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                                     "SELECT ?index, ?max WHERE { \n"
+                                     "<>        :port       ?port. \n"
+                                     "?port     :index      ?index; \n"
+                                     "          :maximum    ?max. }",
+                                     plug_iter->first));
+        while (!results.finished()) {
+          int port_index = atoi(results.get_literal(0));
+          ports[port_index].max = atof(results.get_literal(1));
+          results.next();
+        }
+      }
+      
+      // get default values
+      {
+        RDFResults results(run_query(model, world,
+                                     "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                                     "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                                     "SELECT ?index, ?min WHERE { \n"
+                                     "<>        :port       ?port. \n"
+                                     "?port     :index      ?index; \n"
+                                     "          :default    ?min. }",
+                                     plug_iter->first));
+        while (!results.finished()) {
+          int port_index = atoi(results.get_literal(0));
+          ports[port_index].default_value = atof(results.get_literal(1));
+          results.next();
+        }
+      }
+      
+      // get port hints
+      {
+        RDFResults results(run_query(model, world,
+                                     "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
+                                     "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
+                                     "SELECT ?index, ?hint WHERE { \n"
+                                     "<>        :port       ?port. \n"
+                                     "?port     :index      ?index. \n"
+                                     "?port     :portHint   ?hint. }",
+                                     plug_iter->first));
+        while (!results.finished()) {
+          int port_index = atoi(results.get_literal(0));
+          string hint = results.get_literal(1);
+          if (hint == "<http://lv2plug.in/ns#toggled")
+            ports[port_index].toggled = true;
+          if (hint == "<http://lv2plug.in/ns#integer")
+            ports[port_index].integer = true;
+          if (hint == "<http://lv2plug.in/ns#logarithmic")
+            ports[port_index].logarithmic = true;
+          results.next();
+        }
+      }
+      
+      info[plug_iter->first] = ports;
+    }
+    
+    // write the header file
+    string header_guard = argv[2];
+    for (size_t i = 0; i < header_guard.size(); ++i) {
+      char& c = header_guard[i];
+      if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')))
+        header_guard[i] = '_';
+    }
+    ofstream fout(argv[2]);
+    fout<<"#ifndef "<<header_guard<<endl
+        <<"#define "<<header_guard<<endl<<endl<<endl;
+  
+    // define data structure
+    fout<<"#ifndef PEG_STRUCT"<<endl
+        <<"#define PEG_STRUCT"<<endl
+        <<"typedef struct {"<<endl
+        <<"  float min;"<<endl
+        <<"  float max;"<<endl
+        <<"  float default_value;"<<endl
+        <<"  char toggled;"<<endl
+        <<"  char integer;"<<endl
+        <<"  char logarithmic;"<<endl
+        <<"} peg_data_t;"<<endl
+        <<"#endif"<<endl<<endl;
+    
+    map<string, map<int, PortInfo> >::const_iterator piter;
+    for (piter = info.begin(); piter != info.end(); ++piter) {
+      fout<<"/* "<<piter->first<<" */"<<endl<<endl;
+      
+      // write the URI
+      fout<<"static const char "<<plugins[piter->first]<<"_uri[] = \""
+          <<piter->first<<"\";"<<endl<<endl;
+      
+      // write port labels
+      fout<<"enum "<<plugins[piter->first]<<"_port_enum {"<<endl;
       map<int, PortInfo>::const_iterator iter;
-      int next = 0;
-      for (iter = ports.begin(); iter != ports.end(); ++iter) {
-	if (iter->first != next) {
-	  cerr<<"There was no description of port "<<next
-	      <<" in plugin "<<plug_iter->first<<endl;
-	  return -1;
-	}
-	++next;
+      for (iter = piter->second.begin(); iter != piter->second.end(); ++iter)
+        fout<<"  "<<plugins[piter->first]<<"_"<<iter->second.name<<","<<endl;
+      fout<<"  "<<plugins[piter->first]<<"_n_ports"<<endl
+          <<"};"<<endl<<endl;
+      
+      // write port info
+      fout<<"static const peg_data_t "
+          <<plugins[piter->first]<<"_ports[] = {"<<endl;
+      for (iter = piter->second.begin(); iter != piter->second.end(); ++iter) {
+        fout<<"  { "
+            <<iter->second.min<<", "
+            <<iter->second.max<<", "
+            <<iter->second.default_value<<", "
+            <<(iter->second.toggled ? "1" : "0")<<", "
+            <<(iter->second.integer ? "1" : "0")<<", "
+            <<(iter->second.logarithmic ? "1" : "0")<<" }, "<<endl;
       }
+      fout<<"};"<<endl<<endl<<endl;
+      
     }
     
-    // get min values
-    {
-      RDFResults results(run_query(model, world,
-				   "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-				   "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-				   "SELECT ?index, ?min WHERE { \n"
-				   "<>        :port       ?port. \n"
-				   "?port     :index      ?index; \n"
-				   "          :minimum    ?min. }",
-				   plug_iter->first));
-      if (!results)
-	return -1;
-      while (!results.finished()) {
-	int port_index = atoi(results.get_literal(0));
-	ports[port_index].min = atof(results.get_literal(1));
-	results.next();
-      }
-    }
-    
-    // get max values
-    {
-      RDFResults results(run_query(model, world,
-				   "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-				   "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-				   "SELECT ?index, ?max WHERE { \n"
-				   "<>        :port       ?port. \n"
-				   "?port     :index      ?index; \n"
-				   "          :maximum    ?max. }",
-				   plug_iter->first));
-      if (!results)
-	return -1;
-      while (!results.finished()) {
-	int port_index = atoi(results.get_literal(0));
-	ports[port_index].max = atof(results.get_literal(1));
-	results.next();
-      }
-    }
-    
-    // get default values
-    {
-      RDFResults results(run_query(model, world,
-				   "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-				   "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-				   "SELECT ?index, ?min WHERE { \n"
-				   "<>        :port       ?port. \n"
-				   "?port     :index      ?index; \n"
-				   "          :default    ?min. }",
-				   plug_iter->first));
-      if (!results)
-	return -1;
-      while (!results.finished()) {
-	int port_index = atoi(results.get_literal(0));
-	ports[port_index].default_value = atof(results.get_literal(1));
-	results.next();
-      }
-    }
-    
-    // get port hints
-    {
-      RDFResults results(run_query(model, world,
-				   "PREFIX : <http://lv2plug.in/ns/lv2core#>\n"
-				   "PREFIX ll: <http://ll-plugins.nongnu.org/lv2/namespace#>\n"
-				   "SELECT ?index, ?hint WHERE { \n"
-				   "<>        :port       ?port. \n"
-				   "?port     :index      ?index. \n"
-				   "?port     :portHint   ?hint. }",
-				   plug_iter->first));
-      if (!results)
-	return -1;
-      while (!results.finished()) {
-	int port_index = atoi(results.get_literal(0));
-	string hint = results.get_literal(1);
-	if (hint == "<http://lv2plug.in/ns#toggled")
-	  ports[port_index].toggled = true;
-	if (hint == "<http://lv2plug.in/ns#integer")
-	  ports[port_index].integer = true;
-	if (hint == "<http://lv2plug.in/ns#logarithmic")
-	  ports[port_index].logarithmic = true;
-	results.next();
-      }
-    }
-    
-    info[plug_iter->first] = ports;
+    fout<<"#endif /* "<<header_guard<<" */"<<endl;
+  
   }
   
-  // write the header file
-  string header_guard = argv[2];
-  for (size_t i = 0; i < header_guard.size(); ++i) {
-    char& c = header_guard[i];
-    if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')))
-      header_guard[i] = '_';
+  catch (runtime_error& e) {
+    cerr<<e.what()<<endl;
+    return -1;
   }
-  ofstream fout(argv[2]);
-  fout<<"#ifndef "<<header_guard<<endl
-      <<"#define "<<header_guard<<endl<<endl<<endl;
-  
-  // define data structure
-  fout<<"#ifndef PEG_STRUCT"<<endl
-      <<"#define PEG_STRUCT"<<endl
-      <<"typedef struct {"<<endl
-      <<"  float min;"<<endl
-      <<"  float max;"<<endl
-      <<"  float default_value;"<<endl
-      <<"  char toggled;"<<endl
-      <<"  char integer;"<<endl
-      <<"  char logarithmic;"<<endl
-      <<"} peg_data_t;"<<endl
-      <<"#endif"<<endl<<endl;
-    
-  map<string, map<int, PortInfo> >::const_iterator piter;
-  for (piter = info.begin(); piter != info.end(); ++piter) {
-    fout<<"/* "<<piter->first<<" */"<<endl<<endl;
-    
-    // write the URI
-    fout<<"static const char "<<plugins[piter->first]<<"_uri[] = \""
-        <<piter->first<<"\";"<<endl<<endl;
-    
-    // write port labels
-    fout<<"enum "<<plugins[piter->first]<<"_port_enum {"<<endl;
-    map<int, PortInfo>::const_iterator iter;
-    for (iter = piter->second.begin(); iter != piter->second.end(); ++iter)
-      fout<<"  "<<plugins[piter->first]<<"_"<<iter->second.name<<","<<endl;
-    fout<<"  "<<plugins[piter->first]<<"_n_ports"<<endl
-        <<"};"<<endl<<endl;
-    
-    // write port info
-    fout<<"static const peg_data_t "
-        <<plugins[piter->first]<<"_ports[] = {"<<endl;
-    for (iter = piter->second.begin(); iter != piter->second.end(); ++iter) {
-      fout<<"  { "
-          <<iter->second.min<<", "
-          <<iter->second.max<<", "
-          <<iter->second.default_value<<", "
-          <<(iter->second.toggled ? "1" : "0")<<", "
-          <<(iter->second.integer ? "1" : "0")<<", "
-          <<(iter->second.logarithmic ? "1" : "0")<<" }, "<<endl;
-    }
-    fout<<"};"<<endl<<endl<<endl;
-    
-  }
-  
-  fout<<"#endif /* "<<header_guard<<" */"<<endl;
-  
+
   return 0;
 }
