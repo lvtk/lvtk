3fabf7ece7f0f684bf1d2c10712d719bf785a3ba
Michael Fisher <mfisher31@gmail.com>
2012-09-23 00:17:22 -0500

    Adding StateFlags and StateStatus wrapper enums

--- lv2mm/state.hpp
+++ lv2mm/state.hpp
@@ -27,6 +27,53 @@
 
 namespace LV2 {
 
+   typedef enum {
+      /**
+         Plain Old Data.
+
+         Values with this flag contain no pointers or references to other areas
+         of memory.  It is safe to copy POD values with a simple memcpy and store
+         them for the duration of the process.  A POD value is not necessarily
+         safe to trasmit between processes or machines (e.g. filenames are POD),
+         see LV2_STATE_IS_PORTABLE for details.
+
+         Implementations MUST NOT attempt to copy or serialise a non-POD value if
+         they do not understand its type (and thus know how to correctly do so).
+      */
+      STATE_IS_POD = LV2_STATE_IS_POD,
+
+      /**
+         Portable (architecture independent) data.
+
+         Values with this flag are in a format that is usable on any
+         architecture.  A portable value saved on one machine can be restored on
+         another machine regardless of architecture.  The format of portable
+         values MUST NOT depend on architecture-specific properties like
+         endianness or alignment.  Portable values MUST NOT contain filenames.
+      */
+      STATE_IS_PORTABLE = LV2_STATE_IS_PORTABLE,
+
+      /**
+         Native data.
+
+         This flag is used by the host to indicate that the saved data is only
+         going to be used locally in the currently running process (e.g. for
+         instance duplication or snapshots), so the plugin should use the most
+         efficient representation possible and not worry about serialisation
+         and portability.
+      */
+      STATE_IS_NATIVE = LV2_STATE_IS_NATIVE
+   } StateFlags;
+
+   typedef enum {
+      STATE_SUCCESS         = LV2_STATE_SUCCESS,          /**< Completed successfully. */
+      STATE_ERR_UNKNOWN     = LV2_STATE_ERR_UNKNOWN,      /**< Unknown error. */
+      STATE_ERR_BAD_TYPE    = LV2_STATE_ERR_BAD_TYPE,     /**< Failed due to unsupported type. */
+      STATE_ERR_BAD_FLAGS   = LV2_STATE_ERR_BAD_FLAGS,    /**< Failed due to unsupported flags. */
+      STATE_ERR_NO_FEATURE  = LV2_STATE_ERR_NO_FEATURE,   /**< Failed due to missing features. */
+      STATE_ERR_NO_PROPERTY = LV2_STATE_ERR_NO_PROPERTY   /**< Failed due to missing property. */
+   } StateStatus;
+
   /**
    * The State Feature.
    * The actual type that your plugin class will inherit when you use
@@ -73,20 +120,25 @@ namespace LV2 {
          return 0;
        }
 
-       LV2_State_Status save (LV2_State_Store_Function   store,
-                              LV2_State_Handle           handle,
-                              uint32_t                   flags,
-                              const LV2_Feature *const * features)
-       { return LV2_STATE_SUCCESS; }
+       StateStatus save(LV2_State_Store_Function   store,
+                        LV2_State_Handle           handle,
+                        uint32_t                   flags,
+                        const Feature *const * features)
+       {
+          return STATE_SUCCESS;
+       }
 
-       LV2_State_Status restore (LV2_State_Retrieve_Function retrieve,
-                                 LV2_State_Handle            handle,
-                                 uint32_t                    flags,
-                                 const LV2_Feature *const *  features)
-       { return LV2_STATE_SUCCESS; }
+       StateStatus restore(LV2_State_Retrieve_Function retrieve,
+                           LV2_State_Handle            handle,
+                           uint32_t                    flags,
+                           const Feature *const *  features)
+       {
+          return STATE_SUCCESS;
+       }
 
      protected:
 
+       /** LV2 Api Implementation */
 
        /**
         * @internal
@@ -98,8 +150,8 @@ namespace LV2 {
                                      uint32_t                   flags,
                                      const LV2_Feature *const * features)
        {
-         return reinterpret_cast<Derived*>(instance)->save
-                                               (store, handle,flags,features);
+         Derived* plugin = reinterpret_cast<Derived*>(instance);
+         return (LV2_State_Status)plugin->save(store, handle,flags,features);
        }
 
        /**
@@ -112,13 +164,13 @@ namespace LV2 {
                                         uint32_t                    flags,
                                         const LV2_Feature *const *  features)
        {
-         return reinterpret_cast<Derived*>(instance)->restore
-                                             (retrieve, handle,flags,features);
+         Derived* plugin = reinterpret_cast<Derived*>(instance);
+         return (LV2_State_Status)plugin->restore(retrieve, handle,flags,features);
        }
 
      };
    };
 
-}
+} /* namespace LV2 */
 
 #endif /* LV2_STATE_HPP */
