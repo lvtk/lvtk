4b3d4fb8d80933ae786ad3aca02bf80878962e16
Michael Fisher <mfisher31@gmail.com>
2012-09-23 09:10:00 -0500

    Reviewing for completeness with LV2.  Map and unmap are both working. Cleaning code, style and documenting.

--- lv2mm/urid.hpp
+++ lv2mm/urid.hpp
@@ -27,70 +27,114 @@
 
 namespace LV2 {
 
-  /**
-   * The URID Extension.
-   * The actual type that your plugin class will inherit when you use
-   * this mixin is the internal struct template I.
-   * @ingroup pluginmixins
+   /**
+      The URID Extension.
+      The actual type that your plugin class will inherit when you use
+      this mixin is the internal struct template I.
+      @ingroup pluginmixins
    */
    template <bool Required = true>
    struct URID {
 
-     /** This is the type that your plugin class will inherit when you use the
+      /**
+         This is the type that your plugin class will inherit when you use the
          EventRef mixin. The public and protected members defined here
          will be available in your plugin class.
-     */
-     template <class Derived> struct I : Extension<Required> {
+      */
+      template <class Derived> struct I : Extension<Required> {
 
-       /** @internal */
-       I() { }
+         /** @internal */
+         I() { }
 
-       /** @internal */
-       static void map_feature_handlers(FeatureHandlerMap& hmap) {
-         hmap[LV2_URID__map] = &I<Derived>::handle_map_feature;
-         hmap[LV2_URID__unmap] = &I<Derived>::handle_unmap_feature;
-       }
-
-       /** @internal */
-       static void handle_map_feature(void* instance, void* data) {
-         Derived* d = reinterpret_cast<Derived*>(instance);
-         I<Derived>* fe = static_cast<I<Derived>*>(d);
-
-         fe->p_map = reinterpret_cast<LV2_URID_Map*>(data);
-
-         fe->m_ok = true;
-       }
-
-       /** @internal */
-       static void handle_unmap_feature(void* instance, void* data) {
-         Derived* d = reinterpret_cast<Derived*>(instance);
-         I<Derived>* fe = static_cast<I<Derived>*>(d);
-
-         fe->p_unmap = reinterpret_cast<LV2_URID_Unmap*> (data);
-
-         fe->m_ok = true;
-       }
-
-
-       bool check_ok() {
-         if (LV2CXX_DEBUG) {
-           std::clog<<"    [LV2::URID] Validation "
-                    <<(this->m_ok ? "succeeded" : "failed")<<"."<<std::endl;
+         /** @internal */
+         static void map_feature_handlers(FeatureHandlerMap& hmap)
+         {
+            hmap[LV2_URID__map] = &I<Derived>::handle_map_feature;
+            hmap[LV2_URID__unmap] = &I<Derived>::handle_unmap_feature;
          }
-         return this->m_ok;
-       }
 
+         /** @internal */
+         static void handle_map_feature(void* instance, void* data)
+         {
+            Derived* d = reinterpret_cast<Derived*>(instance);
+            I<Derived>* fe = static_cast<I<Derived>*>(d);
+
+            fe->p_map = reinterpret_cast<LV2_URID_Map*>(data);
+            fe->m_ok = true;
+         }
+
+         /** @internal */
+         static void handle_unmap_feature(void* instance, void* data)
+         {
+            Derived* d = reinterpret_cast<Derived*>(instance);
+            I<Derived>* fe = static_cast<I<Derived>*>(d);
+
+            fe->p_unmap = reinterpret_cast<LV2_URID_Unmap*> (data);
+            fe->m_ok = true;
+         }
+
+
+         bool check_ok()
+         {
+            if (LV2CXX_DEBUG) {
+              std::clog<<"    [LV2::URID] Validation "
+                       <<(this->m_ok ? "succeeded" : "failed")<<"."<<std::endl;
+            }
+            return this->m_ok;
+         }
 
      protected:
 
-       const char* unmap (LV2_URID urid)
-           { return p_unmap->unmap (p_unmap->handle, urid); }
+         /**
+            Get the URI for a previously mapped numeric ID.
 
-       LV2_URID  map (const char* uri)
-           { return p_map->map (p_map->handle, uri); }
+            Returns NULL if @p urid is not yet mapped.  Otherwise, the corresponding
+            URI is returned in a canonical form.  This MAY not be the exact same
+            string that was originally passed to LV2_URID_Map::map(), but it MUST be
+            an identical URI according to the URI syntax specification (RFC3986).  A
+            non-NULL return for a given @p urid will always be the same for the life
+            of the plugin.  Plugins that intend to perform string comparison on
+            unmapped URIs SHOULD first canonicalise URI strings with a call to
+            map_uri() followed by a call to unmap_uri().
 
-       LV2_URID_Map   *p_map;
-       LV2_URID_Unmap *p_unmap;
+            @param urid The ID to be mapped back to the URI string.
+         */
+         const char* unmap (LV2_URID urid)
+         {
+            return p_unmap->unmap (p_unmap->handle, urid);
+         }
+
+         /**
+            Get the numeric ID of a URI.
+
+            If the ID does not already exist, it will be created.
+
+            This function is referentially transparent; any number of calls with the
+            same arguments is guaranteed to return the same value over the life of a
+            plugin instance.  Note, however, that several URIs MAY resolve to the
+            same ID if the host considers those URIs equivalent.
+
+            This function is not necessarily very fast or RT-safe: plugins SHOULD
+            cache any IDs they might need in performance critical situations.
+
+            The return value 0 is reserved and indicates that an ID for that URI
+            could not be created for whatever reason.  However, hosts SHOULD NOT
+            return 0 from this function in non-exceptional circumstances (i.e. the
+            URI map SHOULD be dynamic).
+
+            @param uri The URI to be mapped to an integer ID.
+         */
+         LV2_URID map (const char* uri)
+         {
+            return p_map->map (p_map->handle, uri);
+         }
+
+         LV2_URID_Map   *p_map;
+         LV2_URID_Unmap *p_unmap;
+
+     private:
+
+
 
      };
    };
