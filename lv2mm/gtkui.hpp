a36056572bb850ca0164ee8b5a30ce73d80c0552
Michael Fisher <mfisher31@gmail.com>
2012-09-24 08:36:24 -0500

    Removing unrelated mixins.

--- lv2mm/gtkui.hpp
+++ lv2mm/gtkui.hpp
@@ -34,15 +34,10 @@
 #include <gtkmm.h>
 
 #include <lv2/lv2plug.in/ns/extensions/ui/ui.h>
-#include <lv2/lv2plug.in/ns/ext/presets/presets.h>
-#include <lv2/lv2plug.in/ns/ext/uri-map/uri-map.h>
-#include <lv2/lv2plug.in/ns/ext/event/event-helpers.h>
 
-#include "private/lv2_ui_presets.h"
-#include "private/lv2_osc.h"
-#include "private/debug.hpp"
-
+#include <lv2mm/event.hpp>
 #include <lv2mm/types.hpp>
+#include "private/debug.hpp"
 
 namespace LV2 {
 
@@ -178,185 +173,6 @@ protected:
     };
     
   };
-
-
-  /** Preset GUI extension - the host will tell the GUI what presets are
-      available and which one of them is currently active, the GUI can request 
-      saving and using presets.
-
-      The actual type that your plugin class will inherit when you use 
-      this mixin is the internal struct template I.
-      @ingroup guimixins
-  */
-   LV2MM_MIXIN_CLASS Presets {
-    
-    /** This is the type that your plugin or GUI class will inherit when you 
-	use the	Presets mixin. The public and protected members defined 
-	here will be available in your plugin class.
-    */
-    LV2MM_MIXIN_DERIVED {
-
-        I() : m_hdesc(0), m_host_support(false) { }
-	
-	/** @internal */
-	static void map_feature_handlers(FeatureHandlerMap& hmap) {
-	  hmap[LV2_UI_PRESETS_URI] = &I<Derived>::handle_feature;
-	}
-	
-	/** @internal */
-	static void handle_feature(void* instance, void* data) {
-	  Derived* d = reinterpret_cast<Derived*>(instance);
-	  I<Derived>* e = static_cast<I<Derived>*>(d);
-	  e->m_hdesc = static_cast<LV2UI_Presets_Feature*>(data);
-	  e->m_ok = (e->m_hdesc != 0);
-	  e->m_host_support = (e->m_hdesc != 0);
-	}
-	
-        bool check_ok() {
-	  if (LV2MM_DEBUG) {
-	    std::clog<<"    [LV2::Presets] Validation "
-		     <<(this->m_ok ? "succeeded" : "failed")<<"."<<std::endl;
-	  }
-	  return this->m_ok;
-	}
-	
-	/** This is called by the host to let the GUI know that a new 
-	    preset has been added or renamed.
-	    @param number The number of the added preset.
-	    @param name The name of the added preset.
-	*/
-	void preset_added(uint32_t    number, 
-			   char const* name) {
-	  
-	}
-	
-	/** This is called by the host to let the GUI know that a previously 
-	    existing preset has been removed.
-	    @param number The number of the removed preset.
-	*/
-	void preset_removed(uint32_t number) {
-	  
-	}
-	
-	/** This is called by the host to let the GUI know that all previously
-	    existing presets have been removed. 
-	*/
-	void presets_cleared() {
-	  
-	}
-	
-	/** This is called by the host to let the GUI know that the current 
-	    preset has changed. If the number is equal to 
-	    @c LV2_UI_PRESETS_NOPRESET there is no current preset.
-	    @param number The number of the active preset, or 
-	                  LV2_UI_PRESETS_NOPRESET if there is no active preset.
-	*/
-	void current_preset_changed(uint32_t number) {
-	  
-	}
-	
-	/** @internal
-	    Returns the plugin descriptor for this extension.
-	*/
-	static void const* extension_data(char const* uri) {
-	  static LV2UI_Presets_GDesc desc = { &_preset_added,
-					      &_preset_removed,
-					      &_presets_cleared,
-					      &_current_preset_changed };
-	  if (!std::strcmp(uri, LV2_UI_PRESETS_URI))
-	    return &desc;
-	  return 0;
-	}
-	
-    protected:
-	
-	/** You can call this to request that the host changes the current 
-	    preset to @c preset. */
-	void change_preset(uint32_t preset) {
-	  if (m_hdesc) {
-	    if (LV2MM_DEBUG) {
-	      std::clog<<"[LV2::Presets] change_preset("<<preset<<")"
-		       <<std::endl;
-	    }
-	    m_hdesc->change_preset(static_cast<Derived*>(this)->controller(), 
-				   preset);
-	  }
-	  else if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] change_preset("<<preset<<")"
-		     <<" --- Function not provided by host!"<<std::endl;
-	  }
-	}
-	
-	/** You can call this to request that the host saves the current state
-	    of the plugin instance to a preset with the given number and name.
-	*/
-	void save_preset(uint32_t preset, char const* name) {
-	  if (m_hdesc) {
-	    if (LV2MM_DEBUG) {
-	      std::clog<<"[LV2::Presets] save_preset("<<preset<<", \""
-		       <<name<<"\")"<<std::endl;
-	    }
-	    m_hdesc->save_preset(static_cast<Derived*>(this)->controller(), 
-				 preset, name);
-	  }
-	  else if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] save_preset("<<preset<<", \""
-		     <<name<<"\")"
-		     <<" --- Function not provided by host!"<<std::endl;
-	  }
-	}
-      
-        /** Returns @c true if the host supports the Preset feature, @c false
-	    if it does not. */
-        bool host_supports_presets() const {
-	  return m_host_support;
-	}
-	
-    private:
-	
-	static void _preset_added(LV2UI_Handle gui, 
-				   uint32_t     number, 
-				   char const*  name) {
-	  if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] Host called preset_added("
-		     <<number<<", \""<<name<<"\")."<<std::endl;
-	  }
-	  static_cast<Derived*>(gui)->preset_added(number, name);
-	}
-	
-	static void _preset_removed(LV2UI_Handle gui, 
-				     uint32_t     number) {
-	  if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] Host called preset_removed("
-		     <<number<<")."<<std::endl;
-	  }
-	  static_cast<Derived*>(gui)->preset_removed(number);
-	}
-	
-	static void _presets_cleared(LV2UI_Handle gui) {
-	  if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] Host called presets_cleared()."
-		     <<std::endl;
-	  }
-	  static_cast<Derived*>(gui)->presets_cleared();
-	}
-	
-	static void _current_preset_changed(LV2UI_Handle gui, 
-					     uint32_t     number) {
-	  if (LV2MM_DEBUG) {
-	    std::clog<<"[LV2::Presets] Host called current_preset_changed("
-		     <<number<<")."<<std::endl;
-	  }
-	  static_cast<Derived*>(gui)->current_preset_changed(number);
-	}
-	
-	LV2UI_Presets_Feature* m_hdesc;
-        bool m_host_support;
-	
-    };
-
-  };
-
   
   /** A mixin that allows easy sending of MIDI from GUI to plugin. 
 
@@ -470,73 +286,9 @@ protected:
   };  
   
   
-  /** @internal
-      A mixin that allows easy sending of OSC from GUI to plugin. 
-
-      The actual type that your plugin class will inherit when you use 
-      this mixin is the internal struct template I.
-      
-      Do NOT use this. It may change in the future.
-      @ingroup guimixins
-  */
-   LV2MM_MIXIN_CLASS WriteOSC {
-    
-    /** This is the type that your plugin or GUI class will inherit when you 
-	use the	WriteOSC mixin. The public and protected members defined 
-	here will be available in your plugin class.
-    */
-    LV2MM_MIXIN_DERIVED {
-
-       I() : m_osc_type(0) {
-	m_buffer = lv2_event_buffer_new(sizeof(LV2_Event) + 256, 0);
-      }
-      
-      bool check_ok() {
-	Derived* d = static_cast<Derived*>(this);
-	m_osc_type = d->
-	  uri_to_id(LV2_EVENT_URI, "http://lv2plug.in/ns/ext/osc#OscEvent");
-	m_event_buffer_format = d->
-	  uri_to_id(LV2_UI_URI, "http://lv2plug.in/ns/extensions/ui#Events");
-	return !Required || (m_osc_type && m_event_buffer_format);
-      }
-      
-    protected:
-      
-      bool write_osc(uint32_t port, const char* path, const char* types, ...) {
-	if (m_osc_type == 0)
-	  return false;
-	// XXX handle all sizes here - this is dangerous
-	lv2_event_buffer_reset(m_buffer, 0, m_buffer->data);
-	LV2_Event_Iterator iter;
-	lv2_event_begin(&iter, m_buffer);
-	va_list ap;
-	va_start(ap, types);
-	uint32_t size = lv2_osc_event_vsize(path, types, ap);
-	va_end(ap);
-	if (!size)
-	  return false;
-	va_start(ap, types);
-	bool success = lv2_osc_buffer_vappend(&iter, 0, 0, m_osc_type, 
-					      path, types, size, ap);
-	va_end(ap);
-	if (success) {
-	  static_cast<Derived*>(this)->
-	    write(port, m_buffer->header_size + m_buffer->capacity, 
-		  m_event_buffer_format, m_buffer);
-	  return true;
-	}
-	return false;
-      }
-      
-      uint32_t m_osc_type;
-      uint32_t m_event_buffer_format;
-      LV2_Event_Buffer* m_buffer;
-      
-    };
-    
-  };  
   
   
+
   /** This is the base class for a plugin GUI. You should inherit it and 
       override any public member functions you want to provide 
       implementations for. All subclasses must have a constructor with 
@@ -645,9 +397,13 @@ protected:
     // protected functions in the GUI class, which we want.
     friend class WriteMIDI<true>::I<Derived>;
     friend class WriteMIDI<false>::I<Derived>;
+    
+#if defined (LV2MM_EXTRA_ENABLED) && \
+         defined (USE)
     friend class WriteOSC<true>::I<Derived>;
     friend class WriteOSC<false>::I<Derived>;
-    
+#endif
+
     /** @internal
 	This function creates an instance of a plugin GUI. It is used 
 	as the instantiate() callback in the LV2 descriptor. You should not use
