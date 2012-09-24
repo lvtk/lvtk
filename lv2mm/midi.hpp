1fdd769d33671a9fbd535e87d4054dc0c29226b2
Michael Fisher <mfisher31@gmail.com>
2012-09-24 00:23:48 -0500

    Adding Skeletal MIDI header.

---
+++ lv2mm/midi.hpp
@@ -0,0 +1,32 @@
+/****************************************************************************
+
+    urid.hpp - support file for writing LV2 plugins in C++
+
+    Copyright (C) 2012 Michael Fisher <mfisher31@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
+
+****************************************************************************/
+
+#ifndef LV2_MIDI_HPP
+#define LV2_MIDI_HPP
+
+namespace LV2 {
+
+
+
+}
+
+#endif /* LV2_MIDI_HPP */
