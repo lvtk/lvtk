93a15a58496b317fd01c3a18ee617e9f98cc49d0
Michael Fisher <mfisher31@gmail.com>
2012-12-20 21:51:19 -0600

    Simpler implementation of version tagging.

--- tools/waf/git.py
+++ tools/waf/git.py
@@ -4,10 +4,6 @@
 from subprocess import call
 
 def tag_version(version, msg, prefix="", suffix="", editor="nano"):
-	tag_string = version
-
-	if prefix : tag_string = str(prefix) + tag_string 
-	if suffix : tag_string += str(suffix)
-
+	tag_string = prefix + version + suffix
 	call(["git","tag","-a",tag_string,"-m",msg])
 	return tag_string
