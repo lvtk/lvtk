c2599dfa5ed7374d75494f584ee4cbddd26931b2
Michael Fisher <mfisher31@gmail.com>
2013-02-18 23:03:32 -0600

    Housekeeping

--- lvtk/ext/data_access.hpp
+++ lvtk/ext/data_access.hpp
@@ -1,30 +1,30 @@
-/**
- data_access.hpp - support file for writing LV2 plugins in C++
+/*
+    data_access.hpp - support file for writing LV2 plugins in C++
 
- Copyright (C) 2012 Michael Fisher <mfisher31@gmail.com>
+    Copyright (C) 2012 Michael Fisher <mfisher31@gmail.com>
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
  */
 /**
- @file data_access.hpp
- C++ convenience header for the LV2 data access extension.
- LV2 C Version Support: 1.6 (2012-04-17)
+    @file data_access.hpp
+    C++ convenience header for the LV2 data access extension.
+    LV2 C Version Support: 1.6 (2012-04-17)
 
- This mixin implements the LV2_Extension_Data_Feature.  It provides
- access to LV2_Descriptor::extension_data() for plugin UIs or other
- potentially remote users of a plugin via a data_access() method.
+    This mixin implements the LV2_Extension_Data_Feature.  It provides
+    access to LV2_Descriptor::extension_data() for plugin UIs or other
+    potentially remote users of a plugin via a data_access() method.
  */
 
 #ifndef LVTK_LV2_DATA_ACCESS_HPP
@@ -50,8 +50,6 @@ namespace lvtk
         struct I : Extension<Required>
         {
             I() : p_da(NULL) { }
-
-            /* ================= Mixin API ========================= */
 
             /** @internal */
             static void
@@ -87,8 +85,6 @@ namespace lvtk
                 return this->m_ok;
             }
 
-            /* ================= Data Access C++ Implementation =============== */
-
             /**
              A UI can call this to get data (of a type specified by some other
              extension) from the plugin.
@@ -108,9 +104,7 @@ namespace lvtk
             data_access(const char *uri)
             {
                 if (NULL != p_da)
-                {
                     return p_da->data_access(uri);
-                }
                 return NULL;
             }
 
