7f9d457104511cf062051b0b2e2897384ec702bb
Michael Fisher <mfisher31@gmail.com>
2013-05-09 01:05:42 -0500

    housekeeping, adding lvtk.hpp main include file

--- lvtk/ext/common.h
+++ lvtk/ext/common.h
@@ -7,17 +7,19 @@
 #include <iostream> // For std::clog
 #include <string.h>
 
+#include <lv2/lv2plug.in/ns/lv2core/lv2.h>
+
 #include "lvtk/private/types.hpp"
 
 /* Include all ext's common to plugin and UI */
-
-#include "atom.hpp"
-#include "log.hpp"
-#include "midi.hpp"
-#include "options.hpp"
-#include "patch.hpp"
-#include "urid.hpp"
-#include "time.hpp"
+#include "lvtk/ext/atom.hpp"
+#include "lvtk/ext/log.hpp"
+#include "lvtk/ext/midi.hpp"
+#include "lvtk/ext/options.hpp"
+#include "lvtk/ext/patch.hpp"
+#include "lvtk/ext/units.hpp"
+#include "lvtk/ext/urid.hpp"
+#include "lvtk/ext/time.hpp"
 
 
 #endif /* LVTK_EXT_COMMON_H */
