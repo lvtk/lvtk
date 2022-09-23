7fa88a6bd468de93c5f6b7755a0e3d3eda2757fb
Michael R. Fisher <mfisher@kushview.net>
2022-09-23 02:54:58 -0400

    remove deprecated

--- include/lvtk/option_array.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#warning "<lvtk/option_array.hpp> is deprecated. Use lvtk/options.hpp instead."
-#include <lvtk/options.hpp>
--- include/lvtk/uri_directory.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#warning "<lvtk/symbols.hpp> is deprecated. Use lvtk/symbols.hpp instead."
-#include <lvtk/symbols.hpp>
