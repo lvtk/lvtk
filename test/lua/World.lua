aa0c9b6e8068b5d00933af84879da1a3883b5f9e
Michael R. Fisher <mfisher@kushview.net>
2022-09-24 20:59:37 -0400

    lua.WOrld test: convert to C-style syntax

--- test/lua/World.lua
+++ test/lua/World.lua
@@ -1,23 +1,25 @@
-local World = require ('lvtk.World')
-local w = World.new()
-w:load_all()
+local world = require ('lvtk.World')
+
+local w = world.new()
+world.load_all (w)
 
 local nuris = 0
 local nplus = 0
 
-for k,v in ipairs (w:plugin_uris()) do
+for k,v in ipairs (world.plugin_uris (w)) do
     nuris = nuris + 1
+    print (v)
 end
 
-for k,v in ipairs (w:plugins()) do
+for k,v in ipairs (world.plugins (w)) do
     nplus = nplus + 1
 end
 
 local LVTK_PLUGINS__Volume = "https://lvtk.org/plugins/volume"
-local plugin = w:instantiate (LVTK_PLUGINS__Volume)
+local plugin = world.instantiate (w, LVTK_PLUGINS__Volume)
 
 assert (nuris == nplus and nuris > 0 and plugin ~= nil)
 
-plugin = w:instantiate ("fake plugin")
+plugin = world.instantiate (w, "fake plugin")
 assert (plugin == nil)
 os.exit(0)
